059f6256f735be9a5f1b2da23a4caa6fee9c657a
Dilawer-alee <dilawer2382005@gmail.com>
2024-12-30 00:07:26 +0500

    task

---
+++ Untitled1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int function(int arr[]){
+	int max=arr[0];
+//	int min=[0];
+	for(int i=0; i<arr[i]; i++){
+		if(arr[i]>max){				
+ 	max=arr[i];
+
+		}
+		
+//	}
+}
+	cout<<"max number is:"<<max;
+	return max;
+}
+
+
+int main(){
+	int res;
+   int arr[]={2,3,4,5,6,7};
+  res=function(arr);
+//  cout<<res;
+}
+
+
+
+
+
